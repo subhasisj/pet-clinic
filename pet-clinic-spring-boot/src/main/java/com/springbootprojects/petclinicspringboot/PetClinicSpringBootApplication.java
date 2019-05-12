966b6aa92d2b1b74b587185de256068dfdbd696e
I323570 <I323570@INLN34378207A.apj.global.corp.sap>
2019-05-12 17:47:10 +0530

    First Commit

---
+++ pet-clinic-spring-boot/src/main/java/com/springbootprojects/petclinicspringboot/PetClinicSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springbootprojects.petclinicspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicSpringBootApplication.class, args);
+	}
+
+}
