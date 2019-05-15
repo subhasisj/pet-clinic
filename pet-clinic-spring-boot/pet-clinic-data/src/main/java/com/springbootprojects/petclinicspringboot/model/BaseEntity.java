e6889a237e1ff0c78c8b905f9afd6d0ee627c74b
I323570 <I323570@INLN34378207A.apj.global.corp.sap>
2019-05-16 00:28:28 +0530

    Added BaseEntity with Long id

---
+++ pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/model/BaseEntity.java
@@ -0,0 +1,10 @@
+package com.springbootprojects.petclinicspringboot.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable{
+	
+	private Long id;
+	
+
+}
--- pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/model/Person.java
+++ pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/model/Person.java
@@ -1,6 +1,6 @@
 package com.springbootprojects.petclinicspringboot.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
 	private String firstName;
 	private String lastName;
