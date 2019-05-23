e7af46cf4db3d508280d52e5b717b94b43a8baf4
I323570 <I323570@INLN34378207A.apj.global.corp.sap>
2019-05-24 00:02:51 +0530

    Closes #8 , Closes #9, closes #10

--- pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/model/Owner.java
+++ pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/model/Owner.java
@@ -1,5 +1,19 @@
 package com.springbootprojects.petclinicspringboot.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+	
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
 
 }
