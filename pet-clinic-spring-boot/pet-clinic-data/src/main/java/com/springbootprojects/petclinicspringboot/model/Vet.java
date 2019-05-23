8a8c3f50be68941188e3979fd2a2cb39602d0c62
I323570 <I323570@INLN34378207A.apj.global.corp.sap>
2019-05-24 00:08:34 +0530

    Closes #11

--- pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/model/Vet.java
+++ pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/model/Vet.java
@@ -1,5 +1,17 @@
 package com.springbootprojects.petclinicspringboot.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+	private Set<Speciality> speciality;
+
+	public Set<Speciality> getSpeciality() {
+		return speciality;
+	}
+
+	public void setSpeciality(Set<Speciality> speciality) {
+		this.speciality = speciality;
+	}
 
 }
