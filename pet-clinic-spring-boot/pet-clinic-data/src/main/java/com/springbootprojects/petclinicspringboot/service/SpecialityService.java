45337de5acf02ba4b737252e9a6dd67ba7fd6628
I323570 <I323570@INLN34378207A.apj.global.corp.sap>
2019-05-26 15:52:07 +0530

    Created Speciality Service and Service Map

---
+++ pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/service/SpecialityService.java
@@ -0,0 +1,13 @@
+package com.springbootprojects.petclinicspringboot.service;
+
+import java.util.Set;
+
+import com.springbootprojects.petclinicspringboot.model.Owner;
+import com.springbootprojects.petclinicspringboot.model.Pet;
+import com.springbootprojects.petclinicspringboot.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+	
+	
+	
+}
---
+++ pet-clinic-spring-boot/pet-clinic-data/src/main/java/com/springbootprojects/petclinicspringboot/service/map/SpecialityServiceMap.java
@@ -0,0 +1,39 @@
+package com.springbootprojects.petclinicspringboot.service.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.springbootprojects.petclinicspringboot.model.Speciality;
+import com.springbootprojects.petclinicspringboot.service.SpecialityService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+
+	}
+
+}
